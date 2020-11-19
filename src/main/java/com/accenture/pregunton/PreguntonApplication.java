963e47e2db160c6f906cdb5494846229a91b0c1e
Popovich <marco.a.popovich@accenture.com>
2020-11-19 10:26:36 -0300

    added swagger and package structure organization

--- src/main/java/com/accenture/pregunton/PreguntonApplication.java
+++ src/main/java/com/accenture/pregunton/PreguntonApplication.java
@@ -2,8 +2,10 @@ package com.accenture.pregunton;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class PreguntonApplication {
 
 	public static void main(String[] args) {
