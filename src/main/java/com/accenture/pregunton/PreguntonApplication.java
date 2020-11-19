af87ddc07dd9422ae9b180c3ec867b25146574df
Popovich <marco.a.popovich@accenture.com>
2020-11-19 09:16:45 -0300

    Initial code structure

---
+++ src/main/java/com/accenture/pregunton/PreguntonApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.pregunton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreguntonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreguntonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/pregunton/PreguntonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.pregunton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreguntonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
