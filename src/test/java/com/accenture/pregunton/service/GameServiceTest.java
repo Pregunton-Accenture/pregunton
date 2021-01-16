91927da1910e57004c233ffcd00ed4a8b78b3db5
Marco Antonio Popovich <marco.a.popovich@gmail.com>
2021-01-16 18:48:58 -0300

    Unit test for create game in service layer

--- src/test/java/com/accenture/pregunton/service/GameServiceTest.java
+++ src/test/java/com/accenture/pregunton/service/GameServiceTest.java
@@ -1,4 +1,38 @@
 package com.accenture.pregunton.service;
 
+import com.accenture.pregunton.model.Game;
+import com.accenture.pregunton.repository.GameRepository;
+import com.accenture.pregunton.repository.RuleRepository;
+import com.accenture.pregunton.util.ModelUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.modelmapper.ModelMapper;
+
+import static org.mockito.ArgumentMatchers.*;
+
+@RunWith(MockitoJUnitRunner.class)
 public class GameServiceTest {
+
+    @Mock
+    private GameRepository gameRepository;
+    @Mock
+    private RuleRepository ruleRepository;
+    @Mock
+    private ModelMapper modelMapper;
+
+    @InjectMocks
+    private GameService gameService;
+
+    @Test
+    public void shouldCreateNewGame() {
+        Game game = ModelUtil.createGame();
+        Mockito.when(modelMapper.map(any(), eq(Game.class))).thenReturn(game);
+        gameService.create(ModelUtil.GAME_DTO, ModelUtil.ID);
+        Mockito.verify(gameRepository, Mockito.times(1)).save(game);
+    }
+
 }
