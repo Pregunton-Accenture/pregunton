141b2032eff9b783239d2d019cf6ff5f49e796f9
Popovich26 <marco.a.popovich@gmail.com>
2021-01-28 22:38:45 -0300

    Added generic mapper lists from entitty to dto and vice versa

---
+++ src/main/java/com/accenture/pregunton/config/MapperList.java
@@ -0,0 +1,22 @@
+package com.accenture.pregunton.config;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Component
+public class MapperList {
+
+    public <E, R> List<R> mapToDtoList(List<E> entities, Function<E, R> mapper) {
+        return Optional.ofNullable(entities)
+                .orElse(Collections.emptyList())
+                .stream()
+                .map(mapper)
+                .collect(Collectors.toList());
+    }
+
+}
